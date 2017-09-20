5205d40ae0f33aa27af5ddfcbbd31751a11f7bc4
dthung <dthung1602@gmail.com>
2017-09-20 11:37:13 +0700

    fix bug in window

---
+++ Bash/test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+email='duongthanhhung1998@yahoo.com.vn'
+password='thanhhung1998'
+redirect='https://meganet.com.vn'
+
+curl http://10.10.0.1:3992/wifi/login \
+             --connect-timeout 5 \
+             --max-time 15 \
+             -H "Host: 10.10.0.1:3992" \
+             -H "Referer: http://10.10.0.1:3992/wifi/pre_login/"\
+             -H "Content-Type: application/x-www-form-urlencoded" \
+             -H "Connection: keep-alive" \
+             -H "Upgrade-Insecure-Requests: 1"\
+             --data-urlencode "username=${email}" \
+             --data-urlencode "password=${password}" \
+             --data-urlencode "redirect=${redirect}";
--- Windows-Python/AutoLoginMeganet-Windows.py
+++ Windows-Python/AutoLoginMeganet-Windows.py
@@ -11,7 +11,8 @@
 
 from httplib import HTTPConnection
 from os.path import dirname, abspath
-from urllib import quote as percent_encode
+from urllib import quote_plus as percent_encode
+
 
 path = dirname(abspath(__file__)) + "/"
 
@@ -19,14 +20,13 @@ path = dirname(abspath(__file__)) + "/"
 fin = open(path + "account.txt", "r")
 username = percent_encode(fin.readline().strip())
 password = percent_encode(fin.readline().strip())
-redirect = percent_encode("https://meganet.com.vn")
 fin.close()
 
 # establish connection
 connection = HTTPConnection("10.10.0.1:3992", timeout=10)
 
 # header & body of request
-body = "username=" + username + "&password=" + password + "&redirect=" + redirect
+body = "username=" + username + "&password=" + password + "&redirect="
 headers = {
     "Host": "10.10.0.1:3992",
     "Referer": "http://10.10.0.1:3992/wifi/pre_login/",
@@ -37,4 +37,7 @@ headers = {
 
 # send request
 connection.request("POST", "/wifi/login", body, headers)
+response = connection.getresponse()
+print response.status
+print response.read()
 connection.close()
